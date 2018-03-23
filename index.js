4e97de88decd0e628d7c649fcdd6ff522388d150
Jonathan Dupré <jonathand@newad.com>
2018-03-23 14:10:55 -0400

    menus

--- index.js
+++ index.js
@@ -1,103 +1,148 @@
 #!/usr/bin/env node
 
-var cli = require('inquirer')
+var fs = require('fs')
+var readline = require('readline')
+var cursor = require('cli-cursor')
+var chalk = require('chalk')
+var util = require('util')
+var log = console.log
 
-console.reset = function () {
-  return process.stdout.write('\x1Bc')
+process.stdin.setRawMode(true)
+readline.emitKeypressEvents(process.stdin)
+
+var debug = function () {
+  var args = [].slice.call(arguments).map(util.inspect)
+  return log(chalk.yellow(...args))
 }
 
-function print (text) {
-  console.reset()
-  if (text) process.stdout.write('\n' + text + '\n\n')
-  return Promise.resolve()
+var keylog = function (str, key) {
+  var ctrl = key.ctrl ? 'ctrl + ' : ''
+  var shift = key.shift ? 'shift + ' : ''
+  return ctrl + shift + key.name
 }
 
-var line = new cli.Separator()
-
-var items = [
-  {
-    name: 'Apple',
-    descriptions: [{
-      triggers: [],
-      text: () => {
-        return `A red fruit.`
-      }
-    }]
-  },
-  {
-    name: 'Orange',
-    descriptions: [{
-      triggers: [],
-      text: () => {
-        return `An orange fruit.`
-      }
-    }]
+class Menu {
+  constructor (choices) {
+    this.choices = choices
+    this.current = 0
+    this.lines = ''
   }
-]
 
-var inventory = {
-  items: items.slice(),
-  prompt: function () {
-    console.reset()
+  open () {
+    this.render()
 
-    var goBack = '< Back to menu'
-    var questions = [{
-      type: 'list',
-      name: 'inventory',
-      message: 'Inventory:',
-      choices: [
-        ...this.items,
-        line,
-        goBack
-      ]
-    }]
+    cursor.hide()
 
-    return cli.prompt(questions).then(answers => {
-      if (answers.inventory === goBack) return print()
+    process.stdin.on('keypress', (str, key) => {
+      var min = 0
+      var max = this.choices.length - 1
 
-      var itemName = answers.inventory
-      var item = inventory.items.find(item => item.name === itemName)
+      if (key.name === 'down') {
+        if (this.current === max) return
+        this.current = this.current + 1
+      }
 
-      return print(item.descriptions[0].text()).then(() => inventory.prompt())
+      if (key.name === 'up') {
+        if (this.current === min) return
+        this.current = this.current - 1
+      }
+
+      if (key.name === 'return') {
+        var choice = this.choices[this.current]
+
+        choice.action()
+
+        return
+      }
+
+      this.render()
     })
+  }
+
+  clear () {
+    var numberOfLines = this.lines.split('\n').length
+
+    if (numberOfLines <= 1) return
+
+    readline.moveCursor(process.stdout, 0, -numberOfLines)
+  }
+
+  render () {
+    this.clear()
+
+    var lines = ''
+
+    lines += '\nMain Menu\n\n'
+
+    var highlighted, cursor, color
+
+    this.choices.forEach((choice, index) => {
+      highlighted = this.current === index
+      cursor = highlighted ? '>' : ' '
+      color = highlighted ? chalk.blue : chalk.white
+
+      lines += color(`${cursor} ${index + 1}) ${choice.name}`)
+      if (index !== this.choices.length - 1) lines += '\n'
+    })
+
+    this.lines = lines
+
+    log(lines)
   }
 }
 
 class App {
-  start () {
-    this.scene = {
-      view: function () {
-        return `You're standing in empty space.`
+  constructor () {
+    log('terminu v0.1')
+
+    this.state = {
+      lastSaved: null
+    }
+
+    this.menu = new Menu([{
+      name: 'New',
+      action: function () {
+
       }
-    }
+    }, {
+      name: 'Load',
+      action: function () {
 
-    this.actions = {
-      'Inventory': () => inventory.prompt()
-    }
-
-    this.menu().then(() => {
-      this.start()
-    })
+      }
+    }, {
+      name: 'Exit',
+      action: function () {
+        log('\nok bye!')
+        process.exit()
+      }
+    }])
   }
 
-  menu () {
-    var questions = [{
-      type: 'list',
-      name: 'menu',
-      message: this.scene.view,
-      choices: [
-        'Inventory'
-      ]
-    }]
-
-    return cli.prompt(questions).then(answers => {
-      var choice = answers['menu']
-
-      return this.actions[choice]()
+  start () {
+    process.stdin.on('keypress', (str, key) => {
+      if (key.ctrl && key.name === 'c') return process.exit()
+      // if (['tab', 'escape'].includes(key.name)) return this.menu.open()
+      // if (key.ctrl && 's') return this.save()
     })
+    this.menu.open()
+  }
+
+  save () {
+    debug('save')
+    this.state.lastSaved = (new Date()).toISOString()
+    var serializedState = JSON.stringify(this.state) + '\n'
+
+    fs.appendFileSync('save.txt', `${serializedState}`)
+  }
+
+  load (callback) {
+    debug('load')
+    var serialized = fs.readFileSync('save.txt')
+    var points = serialized.split('\n')
+    var lastIndex = points.length - 1
+    var state = JSON.parse(points[lastIndex])
+    this.state = state
   }
 }
-
-console.reset()
 
 new App().start()
